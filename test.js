945fedbe8fa89e832920611a5e8c1ab217ca06d1
SNGCU24 <snitsc300@caledonian.ac.uk>
2025-07-19 00:16:49 +0100

    Improved tests

--- test.js
+++ test.js
@@ -1,5 +1,38 @@
 // test.js
 
+// Minimal Sprite class for testing
+class Sprite {
+  constructor(type, x, y) {
+    this.type = type;
+    this.x = x;
+    this.y = y;
+  }
+
+  isPreyOf(other) {
+    return (
+      (this.type === 'rock' && other.type === 'paper') ||
+      (this.type === 'paper' && other.type === 'scissors') ||
+      (this.type === 'scissors' && other.type === 'rock')
+    );
+  }
+
+  convertTo(type) {
+    this.type = type;
+  }
+}
+
+// Test isPreyOf()
+console.assert(new Sprite('rock', 0, 0).isPreyOf(new Sprite('paper', 0, 0)) === true, 'rock should be prey of paper');
+console.assert(new Sprite('scissors', 0, 0).isPreyOf(new Sprite('rock', 0, 0)) === true, 'scissors should be prey of rock');
+console.assert(new Sprite('paper', 0, 0).isPreyOf(new Sprite('scissors', 0, 0)) === true, 'paper should be prey of scissors');
+console.assert(new Sprite('paper', 0, 0).isPreyOf(new Sprite('rock', 0, 0)) === false, 'paper should not be prey of rock');
+
+// Test convertTo()
+const s = new Sprite('rock', 0, 0);
+s.convertTo('scissors');
+console.assert(s.type === 'scissors', 'Sprite should convert to scissors');
+
+// Test getWinner function
 function getWinner(a, b) {
   if (a === b) return 'draw';
   if (
